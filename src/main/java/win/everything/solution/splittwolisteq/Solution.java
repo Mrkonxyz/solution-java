d04e9ca76a2c7ffbfe94abd2834269d3e3d06f9e
saranyu thaioprasong <saranyu.tha@ttbbank.com>
2026-09-14 22:10:45 +0700

    refactor to functional programming

--- src/main/java/win/everything/solution/splittwolisteq/Solution.java
+++ src/main/java/win/everything/solution/splittwolisteq/Solution.java
@@ -2,43 +2,51 @@ package win.everything.solution.splittwolisteq;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Solution {
 
-    public record TwoList(List<Integer> l1,List<Integer> l2) {
+    public record TwoList(List<Integer> l1, List<Integer> l2) {
     }
 
     public TwoList splitTwoListEq(List<Integer> org) {
-
-        if (Objects.isNull(org) || org.isEmpty()) {
-            return null;
-        }
-
-        int sum = sumValueInList(org);
-        if (isOdd(sum)) {
-            return null;
-        }
-
-        var mid = sum / 2;
-        var initialPossibleList = new ArrayList<ArrayList<Integer>>();
-        var initialList = new ArrayList<Integer>();
-        initialList.add(org.getFirst());
-        initialPossibleList.add(initialList);
-
-        return Optional.ofNullable(findCorrectList(org, mid, initialPossibleList))
-                .map(correctList -> new TwoList(correctList, differentList(org, correctList)))
+        var toResult = toResult(org);
+        return Optional.ofNullable(org)
+                .filter(this::isNotEmpty)
+                .map(this::sumValueInList)
+                .filter(this::isEven)
+                .map(this::minusTwo)
+                .map(mid -> findCorrectList(org, mid, initialPossibleList(org.getFirst())))
+                .map(toResult)
                 .orElse(null);
     }
 
-    private List<Integer> findCorrectList(List<Integer> org, int target, ArrayList<ArrayList<Integer>> possibleList) {
-        if (possibleList.isEmpty()) {
-            return null;
-        }
+    private ArrayList<ArrayList<Integer>> initialPossibleList(Integer firstElement) {
+        var initialPossibleList = new ArrayList<ArrayList<Integer>>();
+        var initialList = new ArrayList<Integer>();
+        initialList.add(firstElement);
+        initialPossibleList.add(initialList);
+        return initialPossibleList;
+    }
 
-       return Optional.ofNullable(getCorrectList(target, possibleList))
+    private Function<List<Integer>, TwoList> toResult(List<Integer> org) {
+        return correctList -> new TwoList(correctList, differentList(org, correctList));
+    }
+
+    private Integer minusTwo(Integer n) {
+        return n / 2;
+    }
+
+    private boolean isNotEmpty(List<Integer> input) {
+        return input != null && !input.isEmpty();
+    }
+
+    private  List<Integer> findCorrectList(List<Integer> org, int target, ArrayList<ArrayList<Integer>> possibleList) {
+       return possibleList.isEmpty() ? null
+               : Optional.ofNullable(getCorrectList(target, possibleList))
                 .orElse(findCorrectList(org, target, generatePossibleList(org, target, possibleList)));
     }
 
@@ -58,21 +66,30 @@ public class Solution {
     }
 
     private ArrayList<ArrayList<Integer>> findPossibleList(List<Integer> baseList, List<Integer> possibleValue, int target) {
+        var newAndAdd = newArrayAndAndVal(baseList);
+        var isPossible = possibleListCondition(target);
         return possibleValue
                 .stream()
-                .map(v -> {
-                    var possibleList = new ArrayList<>(baseList);
-                    possibleList.add(v);
-                    return possibleList;
-                })
-                .filter( possibleList -> target > 0 ? sumValueInList(possibleList) <= target : sumValueInList(possibleList) >= target)
+                .map(newAndAdd)
+                .filter(isPossible)
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
+    private Predicate<List<Integer>> possibleListCondition(int target) {
+        return possibleList -> target > 0 ? sumValueInList(possibleList) <= target : sumValueInList(possibleList) >= target;
+    }
 
-    private boolean isOdd(int n) {
+    private Function<Integer  , ArrayList<Integer>> newArrayAndAndVal(List<Integer> baseList) {
+        return v -> {
+            var newList = new ArrayList<>(baseList);
+            newList.add(v);
+            return newList;
+        };
+    }
+
+    private boolean isEven(int n) {
         n = n < 0 ? n * -1 : n;
-        return n % 2 == 1;
+        return n % 2 == 0;
     }
 
     private int sumValueInList(List<Integer> l1) {
@@ -80,10 +97,7 @@ public class Solution {
     }
 
     private int sumValueInList(List<Integer> l1, int sum) {
-        if (l1.isEmpty()) {
-            return sum;
-        }
-        return sumValueInList(l1.subList(1, l1.size()), sum + l1.getFirst());
+        return l1.isEmpty() ? sum : sumValueInList(l1.subList(1, l1.size()), sum + l1.getFirst());
     }
 
     private <T> ArrayList<T> differentList(List<T> l1, List<T> l2) {
